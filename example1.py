766d9a3b4fbdcb788f9adfddc3c1b46be5881b53
Sunil Shirur <sshirur@users.noreply.github.com>
2024-01-22 17:50:34 +0530

    Create example1.py

---
+++ example1.py
@@ -0,0 +1,6 @@
+#This program illustarte input(), int(), str(), ==, if else, while, break, continue, for and range functions
+
+#input(), str() and variables 
+print('enter your name)
+myName=str(input()) # This line illustrates the input function
+print('your name is: ' +myName) 
